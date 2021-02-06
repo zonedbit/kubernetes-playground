96d3a1d2e76d2a757c01ed999c3028eaa7a3e7e3
zonedbit <zonedbit@protonmail.com>
2021-02-06 14:30:09 +0100

    feature/updateKubeConfigScript

---
+++ updateKubeConfig.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+KUBE_CONF_DIR="./.kubeconf"
+KUBE_CONF_FILE="config"
+KUBE_CONFIGURATION="$KUBE_CONF_DIR/$KUBE_CONF_FILE"
+
+K8S_DOMAIN="k8s\.home"
+
+# create the config directory
+if [ ! -d "$KUBE_CONF_DIR" ]; then
+  mkdir $KUBE_CONF_DIR
+fi
+
+# Copy the configuration to the host
+vagrant ssh -c microk8s.config > "$KUBE_CONFIGURATION"
+
+# replace the IP with the DNS name
+sed -i -r 's/([0-9]{1,3}\.){3}[0-9]{1,3}/k8s\.home/g' "$KUBE_CONFIGURATION"
+
+# Add the configuration to the path
